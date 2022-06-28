50ce2b85c76189e21d43e44032ec2b8dd29de1f0
Caleb Holm <calebcholm@gmail.com>
2022-06-27 21:41:10 -0500

    Finished dishes controller and router

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,8 +11,95 @@ function list(req, res) {
   const { orderId } = req.params;
   res.json({
     data: dishes.filter(
-      orderId ? (dish) => dish.id === Number(orderId) : () => true
+      orderId ? dish => dish.id === Number(orderId) : () => true
     ),
   });
 }
 
+//id, name, description, price, image_url
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include property: '${propertyName}'` });
+  };
+}
+
+function create(req, res) {
+  const { data: { id, name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find(dish => dish.id === dishId);
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found: '${dishId}'`,
+  });
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function update(req, res) {
+  const dish = res.locals.dish;
+  const {
+    data: { name, description, price, image_url },
+  } = req.body;
+
+  dish.name = name;
+  dish.description = description;
+  dish.price = price;
+  dish.image_url = image_url;
+
+  res.json({ data: dish });
+}
+
+function priceIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || typeof price !== 'number') {
+    return next({
+      status: 400,
+      message: `price must be a number greater than 0`,
+    });
+  }
+  next();
+}
+
+module.exports = {
+  create: [
+    bodyDataHas('name'),
+    bodyDataHas('description'),
+    bodyDataHas('price'),
+    bodyDataHas('image_url'),
+    priceIsValid,
+    create,
+  ],
+  list,
+  read: [dishExists, read],
+  update: [
+    dishExists,
+    bodyDataHas('name'),
+    bodyDataHas('description'),
+    bodyDataHas('price'),
+    bodyDataHas('image_url'),
+    priceIsValid,
+    update,
+  ],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,68 @@
-const path = require("path");
+const path = require('path');
 
 // Use the existing order data
-const orders = require(path.resolve("src/data/orders-data"));
+const orders = require(path.resolve('src/data/orders-data'));
 
 // Use this function to assigh ID's when necessary
-const nextId = require("../utils/nextId");
+const nextId = require('../utils/nextId');
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+function list(req, res) {
+  res.json({ data: orders });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include property: '${propertyName}'` });
+  };
+}
+
+//id, deliverTo, mobileNumber, status, dishes[]
+function create(req, res) {
+  const { data: { id, deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+function orderExists(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find(order => order.id === orderId);
+  if (foundOrder) {
+    res.locals.order = foundOrder;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Order id not found: ${orderId}`,
+  });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.order });
+}
+//id, deliverTo, mobileNumber, status, dishes[]
+module.exports = {
+  list,
+    read: [orderExists, read],
+    create: [
+        bodyDataHas('id'),
+        bodyDataHas('deliverTo'),
+        bodyDataHas('mobileNumber'),
+        bodyDataHas('status'),
+        bodyDataHas('dishes'),
+        create,
+  ],
+  orderExists,
+};
